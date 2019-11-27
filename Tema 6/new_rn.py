33f073b651f3c9a524243ac9357b0c67536c64d8
Andrei2498 <48515205+Andrei2498@users.noreply.github.com>
2019-11-28 01:16:53 +0200

    Update new_rn.py

--- Tema 6/new_rn.py
+++ Tema 6/new_rn.py
@@ -1,5 +1,6 @@
 import numpy
 import random
+import math
 
 # Aici stocam valorile modificate.
 w_matrix_input = []
@@ -59,7 +60,7 @@ def first_initialization():
     error_matrix = [[] for i in range(0, len(output_expected[0]))]
 
 
-def start(numar_neuroni_strat_ascuns: int, rata_de_invatare: float,eroare_maxima: float, numar_epoci: int ):
+def start(numar_neuroni_strat_ascuns: int, rata_de_invatare: float,eroare_maxima: float, numar_epoci: int):
     global hidden_layer_number, input_number
     hidden_layer_number = numar_neuroni_strat_ascuns
     populate_input_and_expected_output()
@@ -71,23 +72,49 @@ def start(numar_neuroni_strat_ascuns: int, rata_de_invatare: float,eroare_maxima
         antrenare_epoca(rata_de_invatare)
         if MSE < eroare_maxima:
             break
+    
 
+
+def suma_gradient_input(gradient_list, j):
+    suma = 0
+    for i in range(0, len(gradient_list)):
+        suma += gradient_list[i] * w_matrix_output[j][i]
+    return suma
 
 def antrenare_epoca(rata_de_invatare: float):
+    global MSE
     for i in range(0, input_number):
 
         # Calculare y de pe stratul ascuns, adica iesirile reale de pe stratul ascuns
+        print("Crapa aici 1")
         input_y_list = numpy.dot(input_tested[i], w_matrix_input)
+        print("Crapa aici 1.5")
         for j in range(0, len(input_y_list)):
             input_y_list[j] = 1 / numpy.exp(-(input_y_list[j] - bias_list_input[j]))
 
+        print("Crapa aici 2")
         # Calculare y de pe stratul final, adica iesirile reale de pe stratul final/output
         output_y_list = numpy.dot(input_y_list, w_matrix_output)
         for j in range(0, len(output_y_list)):
             output_y_list[j] = 1 / numpy.exp(-(output_y_list[j] - bias_list_output[j]))
 
-        # Calculare eroare pt inputul curent
+        # Calculare eroare pt inputul curent, calcularea gradientului erorii, update w_output si bias_output
+        gradient_list = []
         for j in range(0, len(output_y_list)):
             error_matrix[i][j] = output_expected[i][j] - output_y_list[j]
+            MSE += math.pow(error_matrix[i][j], 2)
+            gradient_list[j] = output_y_list[j] * (1 - output_y_list) * error_matrix[i][j]
+            for k in range(0, hidden_layer_number):
+                w_matrix_output[k][j] = rata_de_invatare * input_y_list[k] * gradient_list[j]
+            bias_list_output[j] = - (rata_de_invatare * gradient_list[j])
+
+        #Calculare gradienti,w_input,bias_input
+        input_gradient_list = []
+        for j in range(0, len(input_y_list)):
+            input_gradient_list[j] = input_y_list[j] * (1 - input_y_list) * suma_gradient_input(gradient_list, j)
+            for k in range(0, input_number):
+                w_matrix_input[k][j] += rata_de_invatare * input_tested[i][k] * input_gradient_list[j]
+            bias_list_input[j] += -(rata_de_invatare * input_gradient_list[j])
+
 
         
